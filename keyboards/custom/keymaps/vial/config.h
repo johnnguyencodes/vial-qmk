8a601aacb09d6059e57324d288e91ac32d67c8b5
realet <ectrink@gmail.com>
2022-05-04 16:01:43 -0400

    upload working copy

---
+++ keyboards/custom/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xB5, 0x3C, 0x11, 0x8A, 0x50, 0x70, 0x30, 0xFD}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 1, 10 }
+#define VIAL_UNLOCK_COMBO_COLS { 1, 10 }
